f8ced04c09de694ab922fbf6d2368badf0b79550
Firtch <firtch@gmail.com>
2021-05-29 02:30:26 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  nums = [1, 2, 3, 4, 5];
+
+  render() {
+    return (
+      <div className="App">
+        {this.nums.map((n) => {
+          return <span>{n + 3}</span>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
